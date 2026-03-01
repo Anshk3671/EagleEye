f094eb441ba3f118fc64abaa1c0f38cdf4d358ed
Ansh Kumar <Anshk3671@gmail.com>
2026-03-01 10:15:00 +0530

    Initial project scaffold with Express server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Database Initialization
+const db = require('./src/database/db');
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`EagleEye Logistics Server running on port ${PORT}`);
+});
